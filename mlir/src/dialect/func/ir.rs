8231a003731a55b7067f7e063e17bd64fb9a9872
Rin Arakaki <rnarkkx@gmail.com>
2023-11-14 23:45:47 +0900

    Update ir.rs

--- mlir/src/dialect/func/ir.rs
+++ mlir/src/dialect/func/ir.rs
@@ -1,6 +1,4 @@
-/*!
-- include <https://github.com/llvm/llvm-project/tree/main/mlir/include/mlir/Dialect/Func/IR>
-- lib <https://github.com/llvm/llvm-project/tree/main/mlir/lib/Dialect/Func/IR>
-*/
+//! - include <https://github.com/llvm/llvm-project/tree/main/mlir/include/mlir/Dialect/Func/IR>
+//! - lib <https://github.com/llvm/llvm-project/tree/main/mlir/lib/Dialect/Func/IR>
 
 pub mod operations;
